25370f43904b1649586a0fbfd0d3b19fa95b2766
charlieCompton <ccompton@lclark.edu>
2023-05-13 10:28:24 -0700

    fixing some stuff

--- website-app/src/App.js
+++ website-app/src/App.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
 import HomeBody from './components/HomeBody';
 import About from './components/About'
-import Projects from './components/Pojects'
+import Projects from './components/Projects'
 import ReachOut from './components/ReachOut'
 
 function App() {
---
+++ website-app/src/components/ReachOut.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './ReachOut.css'
+
+const ReachOut = () => {
+    return(
+        <div className="container">
+            <h1>contact</h1>
+        </div>
+    );
+}
+
+export default ReachOut;
