435a6dabc4e4d348ce6020c6a511a53d6a0ea679
charlieCompton <ccompton@lclark.edu>
2023-06-28 13:01:46 -0700

    so everything is looking good so far. I need to make some stylistic changes to the h1 and p tags,and maybe switch the column they're in to a col-sm-7 instead of col-sm-6. once that is done I need to create a nav bar, create a fade in effect for everything ontop of the circles, move the pdf download, change styles of buttons, fix the carousel, make sure the links work, add EmailJS, and throw some socials at the bottom, then everything will be done

--- website-app/src/components/HomeBody/HomeBody.js
+++ website-app/src/components/HomeBody/HomeBody.js
@@ -1,31 +1,49 @@
 import React from "react";
 import './HomeBody.css';
-import BubbleBlur from "./Background";
 
 const DownloadPdf = () => {
     const pdfLink = 'src/Files/Charlie Compton.pdf';
-
     const link = document.createElement('a');
     link.href = pdfLink;
     link.download = 'Charlie Compton\'s resume.pdf';
     link.target = '_blank';
-
     document.body.appendChild(link);
-
     link.click();
-
     document.body.removeChild(link);
-
 }
 
 const HomeBody = () => {
     return (
         <>
-            <BubbleBlur />
-            <div className="m-2" id="resumeOrHobbies">
-                    <p id="downloadParagraph"></p>
-                    <button id="resumeButton" onClick={DownloadPdf}>Download</button>
+        <div className="row p-0 m-0" id="BGBubble">
+            <div className="col p-0 m-0">
+                <div className="position-relative z-index-1">
+                    <div id="circle1"></div>
+                    <div id="circle2"></div>
+                    <div id="circle3"></div>
+                    <div id="circle4"></div>
+                </div>
             </div>
+            <div className="container">
+                <div className="row p-0 m-0">
+                    <div className="col-sm-6 p-2" id="aboutMe">
+                        <div className="position-relative z-index-2">
+                            <h1>Hello, My Name is Charlie Compton</h1>
+                            <p>
+                                About Me: I'm a recent college graduate with interests in UI design,
+                                Software Development, and more! This also needs a better font, maybe can do
+                                a different font collor or something interesting
+                                links below, like my resume 
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="m-2" id="resumeOrHobbies">
+                <p id="downloadParagraph"></p>
+                <button id="resumeButton" onClick={DownloadPdf}>Download</button>
+        </div>
         </>
     );
 }
